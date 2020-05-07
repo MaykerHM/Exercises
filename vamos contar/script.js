2f81e8afdcd5d6626c82df9ed2c71e4b16da5cd4
Mayker Miyanaga <hydeki8@gmail.com>
2020-05-07 00:08:37 -0300

    exercises

--- vamos contar/script.js
+++ vamos contar/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function contar() {
+    var iniciotxt = document.querySelector('input#inicio').value
+    var fimtxt = document.querySelector('input#fim').value
+    var passotxt = document.querySelector('input#passo').value
+
+    var inicio = Number(iniciotxt)
+    var fim = Number(fimtxt)
+    var passo = Number(passotxt)
+
+    var res = document.querySelector('div#res')
+    var resultado = 0
+
+    for (inicio; inicio >= fim; inicio = inicio + passo) {
+        resultado += inicio
+    }
+
+    res.innerHTML = String(resultado)
+    console.log(typeof(inicio))
+}
